37430d8cd9083058c6406189dd7fa9521b83e7e0
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-21 15:29:30 -0300

    added create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,17 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
 
-  def posts_params
+  def post_params
     params.require(:post).permit(:comment)
   end
 end
