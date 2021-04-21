bb5a3c1be82a3617a92143639bd9a3675b511e0a
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-21 15:26:35 -0300

    coded strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,14 @@ class PostsController < ApplicationController
     @new_post = Post.new
     @all_posts = Post.order(created_at: :desc).all
   end
+
+  def create
+
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:comment)
+  end
 end
